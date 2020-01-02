63d45bec58522a086f0a86b80909564099f6f0c6
Fred Wills <willsf422@gmail.com>
2020-01-02 06:55:29 -0500

    celcius farenheit convertor

---
+++ celcius-calculator/celcius-calculator.js
@@ -0,0 +1,11 @@
+
+
+ const convert=() =>{
+    let c = Number(document.getElementById("out").value);
+    let f = (c * 9/5) +32;
+    document.getElementById("out").value = f.toString();
+}
+
+
+
+
